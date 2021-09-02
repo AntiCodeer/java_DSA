242cef1f6c44922ee2d873c572b9a77e3aee74fb
AntiCodeer <akmukherjee35@gmail.com>
2021-09-02 11:58:34 +0530

    Solverd Q2

---
+++ Q2_caseCheck.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Q2_caseCheck
+{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nEnter a Cheracter :- ");
+        char c = sc.next().trim().charAt(0);
+
+        if(c >= 'a' && c <= 'z')
+        {
+            System.out.printf("\n %c is a Lowercase Character.",c);
+        }
+        else
+        {
+            System.out.printf("\n %c is a Uppercase Character.",c);
+        }
+        System.out.println();
+    }
+}
