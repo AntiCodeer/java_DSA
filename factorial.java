86375072d3cc626d79cc3fe13e1d8d0c2e05dd13
AntiCodeer <akmukherjee35@gmail.com>
2021-10-19 20:16:44 +0530

    Input a number and print all the factors of that number (use loops).

---
+++ factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class factorial {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        System.out.print("\nEnter Your Number :- ");
+        int n = sc.nextInt();
+        int count = 0;
+        for(int i = 1; i <= n; ++i) {
+            if(n % i == 0) {
+                ++count;
+                System.out.printf("\n%d is a Facotor of %d", i, n);
+            }
+        }
+        System.out.printf("\n%d has Total %d No of Facotors",n, count);
+        System.out.println();
+    }
+}
