a9c5afd7b34994090b7be0d3cff026a8383d56fe
AntiCodeer <akmukherjee35@gmail.com>
2021-10-28 16:05:07 +0530

    Solved Q16

---
+++ Q16_hcflcm.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Q16_hcflcm {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("This Program Will Calculate HCF and LCM of A and B.....");
+        System.out.print("\nEnter the Value of A :- ");
+        int a = in.nextInt();
+        System.out.print("\nEnter the Value of B :- ");
+        int b = in.nextInt();
+        int x = a, y = b;
+        while(y!=0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+        int hcf = x;
+        int lcm = (a*b)/hcf;
+        System.out.printf("\nHCF = %d",hcf);
+        System.out.printf("\nLCM = %d",lcm);
+        System.out.println();
+    }
+}
+        
