c3046de6fea85e022d7d8b7819391355677d8007
AntiCodeer <akmukherjee35@gmail.com>
2021-10-29 13:56:21 +0530

    Solved Q19

---
+++ Q19_leap.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Q19_leap {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("\nEnter a Year :- ");
+		int year = sc.nextInt();
+
+		if (((year%4==0) && (year%100!=0)) || (year%400==0)) {
+			System.out.printf("\n%d is a Leap Year",year);
+		}
+		else {
+			System.out.printf("\n%d is Not a Leap Year",year);
+		}
+		System.out.println();
+	}
+}
