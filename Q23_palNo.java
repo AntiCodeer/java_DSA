d955c810c7dfe1adb8c1bee48bf7c8e1f8c37788
AntiCodeer <akmukherjee35@gmail.com>
2021-10-29 14:05:50 +0530

    Solved Q23

---
+++ Q23_palNo.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Q23_palNo {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("\nEneter a No :- ");
+		int a = in.nextInt();
+
+		if (isPalindrome(a)) {
+			System.out.printf("\n%d is a Palindrome No.",a);
+		}
+		else
+			System.out.printf("\n%d is Not a Palindrome No.",a);
+		System.out.println();
+	}
+
+	static boolean isPalindrome(int n){
+		int rev=0,rem,c=n;
+		while(c!=0){
+			rem = c % 10;
+			rev = (rev*10) + rem;
+			c = c/10;
+		}
+
+		if(rev==n){
+			return true;
+		}
+		else
+			return false;
+	}
+}
