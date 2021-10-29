ef9172d470ddb7e4a552174b073f14c3ce7480b7
AntiCodeer <akmukherjee35@gmail.com>
2021-10-29 16:48:52 +0530

    Solved Q26

---
+++ Q26_sumNatural.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Q26_sumNatural {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		System.out.printf("\nEnter How Many No You Want :- ");
+		int n = in.nextInt();
+
+		sum(n);
+		in.close();
+		System.out.println();
+	}
+
+	static void sum(int n) {
+		int sum=0;
+
+		sum = (n * (n+1))/2;
+		System.out.printf("\nSum = %d",sum);
+	}
+}
