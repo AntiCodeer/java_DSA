722f3a05317f0c2699f90b194b9887fc85fc93f3
AntiCodeer <akmukherjee35@gmail.com>
2021-10-29 13:52:30 +0530

    Solved Q18

---
+++ Q18_Perfect.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Q18_Perfect {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.printf("\nEnter an Integer :- ");
+		int n = sc.nextInt();
+		int sum=0;
+		for (int i=1; i<n; ++i) {
+			if(n%i==0){
+				System.out.printf("\n%d is a Divisor of %d",i,n);
+				sum += i;
+			}
+		}
+		System.out.println();
+		if(sum == n) {
+			System.out.printf("+\n-------------------------\n");
+			System.out.printf("= %d is a Perfect No.",n);
+		}
+		else {
+			System.out.printf("+\n-------------------------\n");
+			System.out.printf("\n!= %d is Not a Perfect No.",n);
+		}
+		System.out.println();
+	}
+}
