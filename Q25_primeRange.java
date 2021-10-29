5763eaf39714a974085c1ced3fb9c96a1d9671f8
AntiCodeer <akmukherjee35@gmail.com>
2021-10-29 14:49:30 +0530

    Solved Q25

---
+++ Q25_primeRange.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Q25_primeRange {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		System.out.printf("\nEnter The Range of Prime No With A and B.....");
+		System.out.printf("\nEnter A -> ");
+		int a = in.nextInt();
+		System.out.printf("\nEnter B -> ");
+		int b = in.nextInt();
+		for (int i=a; i<=b; ++i) {
+			prime(i);
+		}
+		System.out.println();
+	}
+
+	static void prime(int n) {
+		int i;
+		for(i=2; i<=n/2; ++i) {
+			if (n%i==0) {
+				break;
+			}
+		}
+		if (i>n/2) {
+			System.out.printf("\n%d is a Prime No.",n);
+		}
+	}
+}
