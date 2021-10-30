58e21e3ac723921d0c22dd3b45fd267565ea934e
AntiCodeer <akmukherjee35@gmail.com>
2021-10-30 14:39:23 +0530

    Solved Q28

---
+++ Q28_leetCodeSol3.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Q28_leetCodeSol3 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int nums[] = new int [4];
+		for (int i=0; i<nums.length; ++i) {
+			nums[i] = sc.nextInt();			
+		}
+		getConcatenation(nums);
+		sc.close();
+		System.out.println();
+	}
+	static void getConcatenation(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[2*n];
+
+        for (int i=0; i<n; ++i) {
+        	ans[i] = nums[i];
+        	ans[i+n] = nums[i];
+        }
+        System.out.println(Arrays.toString(ans));
+    }
+}
