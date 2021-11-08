cbef4f82a148bb2c5fe0c9a972a1c396846be9d0
AntiCodeer <akmukherjee35@gmail.com>
2021-11-08 12:31:00 +0530

    Solved Q35

---
+++ Q35_leetCodeSol10.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Q35_leetCodeSol10  {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int nums[] = new int [3];
+		for (int i=0; i<nums.length; ++i) {
+			nums[i] = sc.nextInt();
+		}
+		isGoodArray(nums);
+		sc.close();
+		System.out.println();
+	}
+	static void isGoodArray(int[] nums) {
+        int x=nums[0];
+        for (int i=0; i<nums.length; ++i) {
+        	x=gcd(x,nums[i]);        	
+        }
+        if (x==1) {
+        	System.out.println("True");        	
+        }
+        else {
+        	System.out.println("False");
+        }
+
+        // return x==1; //For leetcode
+    }
+    static int gcd(int a,int b) {
+    	while(b>0) {
+    		int temp = b;
+    		b = a % b;
+    		a = temp;
+    	}
+    	return a;
+    }
+}
+
