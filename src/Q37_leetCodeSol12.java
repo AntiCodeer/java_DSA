aa095ab639e48aa3168d3ccd54ca09fef1d046f1
AntiCodeer <akmukherjee35@gmail.com>
2021-11-28 10:12:21 +0530

    Solved Q37

---
+++ src/Q37_leetCodeSol12.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Q37_leetCodeSol12 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String s = sc.nextLine();
+    checkPangram(s);
+    sc.close();
+    System.out.println(checkPangram(s));
+  }
+  static boolean checkPangram(String s) {
+    boolean []a = new boolean[26];
+    for (int i=0; i<s.length(); ++i) {
+      a[s.charAt(i)-'a'] = true;
+    }
+    for (int i=0; i<26; ++i) {
+      if (a[i] == false) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
